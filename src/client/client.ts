a4d9ec407cf9fb765f79cc2eb09a7d4017a986bd
Santiago Loureiro <sloureiro1@lenovo.com>
2023-08-09 18:38:54 -0300

    object3D hierarchy and GUI tutorials

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,36 +1,60 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
 const camera = new THREE.PerspectiveCamera(
-    90,
+    75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 )
-camera.position.z = 3
-
-scene.background = new THREE.Color("#04151F")
-
+camera.position.x = 4
+camera.position.y = 4
+camera.position.z = 4
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const orbitControls = new OrbitControls(camera, renderer.domElement)
-orbitControls.addEventListener("change", render)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.target.set(8, 0, 0)
 
-const geometry = new THREE.TorusGeometry
-const material = new THREE.MeshBasicMaterial({
-    color: "#f2cc8f"
-    //wireframe: true,
-})
+const light1 = new THREE.PointLight()
+light1.position.set(0,1,0)
+scene.add(light1)
 
-const object = new THREE.Mesh(geometry, material)
-scene.add(object)
+const light2 = new THREE.PointLight()
+light2.position.set(0,1,0)
+light2.intensity = 80
+scene.add(light2)
 
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    new THREE.MeshPhongMaterial({ color: 0xff0000 })
+)
+object1.position.set(4, 0, 0)
+scene.add(object1)
+object1.add(new THREE.AxesHelper(5))
+
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+)
+object2.position.set(4, 0, 0)
+object1.add(object2)
+object2.add(new THREE.AxesHelper(5))
+
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    new THREE.MeshPhongMaterial({ color: 0x0000ff })
+)
+object3.position.set(4, 0, 0)
+object2.add(object3)
+object3.add(new THREE.AxesHelper(5))
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -40,16 +64,71 @@ function onWindowResize() {
     render()
 }
 
+const gui = new GUI()
+const object1Folder = gui.addFolder('Object1')
+object1Folder.add(object1.position, 'x', 0, 10, 0.01).name('X Position')
+object1Folder
+    .add(object1.rotation, 'x', 0, Math.PI * 2, 0.01)
+    .name('X Rotation')
+object1Folder.add(object1.scale, 'x', 0, 2, 0.01).name('X Scale')
+object1Folder.open()
+const object2Folder = gui.addFolder('Object2')
+object2Folder.add(object2.position, 'x', 0, 10, 0.01).name('X Position')
+object2Folder
+    .add(object2.rotation, 'x', 0, Math.PI * 2, 0.01)
+    .name('X Rotation')
+object2Folder.add(object2.scale, 'x', 0, 2, 0.01).name('X Scale')
+object2Folder.open()
+const object3Folder = gui.addFolder('Object3')
+object3Folder.add(object3.position, 'x', 0, 10, 0.01).name('X Position')
+object3Folder
+    .add(object3.rotation, 'x', 0, Math.PI * 2, 0.01)
+    .name('X Rotation')
+object3Folder.add(object3.scale, 'x', 0, 2, 0.01).name('X Scale')
+object3Folder.open()
+
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+const debug = document.getElementById('debug1') as HTMLDivElement
+
 function animate() {
     requestAnimationFrame(animate)
-
-
+    controls.update()
     render()
+    const object1WorldPosition = new THREE.Vector3()
+    object1.getWorldPosition(object1WorldPosition)
+    const object2WorldPosition = new THREE.Vector3()
+    object2.getWorldPosition(object2WorldPosition)
+    const object3WorldPosition = new THREE.Vector3()
+    object3.getWorldPosition(object3WorldPosition)
+    debug.innerText =
+        'Red\n' +
+        'Local Pos X : ' +
+        object1.position.x.toFixed(2) +
+        '\n' +
+        'World Pos X : ' +
+        object1WorldPosition.x.toFixed(2) +
+        '\n' +
+        '\nGreen\n' +
+        'Local Pos X : ' +
+        object2.position.x.toFixed(2) +
+        '\n' +
+        'World Pos X : ' +
+        object2WorldPosition.x.toFixed(2) +
+        '\n' +
+        '\nBlue\n' +
+        'Local Pos X : ' +
+        object3.position.x.toFixed(2) +
+        '\n' +
+        'World Pos X : ' +
+        object3WorldPosition.x.toFixed(2) +
+        '\n'
+    stats.update()
 }
 
 function render() {
     renderer.render(scene, camera)
 }
 
-// animate()
-render()
+animate()
